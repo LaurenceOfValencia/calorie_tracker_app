b851f059c2711305b44d40911d6a929e576ff844
Lester Laurence Z. Corpuz <lester19.corpuz28@gmail.com>
2022-01-15 11:59:06 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,20 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+TextEditingController ageController = TextEditingController();
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final VoidCallback onPressed;
+  var _selectedGender = "male";
+  String dropdownvalue = 'Light';
+  var items = ['Light', 'Moderate', 'Active', 'Very Active'];
 
   @override
   Widget build(BuildContext context) {
@@ -30,47 +42,100 @@ class HomePage extends StatelessWidget {
           Padding(
             padding: EdgeInsets.symmetric(vertical: 10),
             child: Container(
-              height: 200,
+              height: 510,
               color: Colors.grey[200],
-              child: Center(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(50),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Enter a search term',
-                        ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 5),
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      controller: ageController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Age (15-80)',
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 5),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Height in cm',
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 5),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Weight in kg',
+                      ),
+                    ),
+                  ),
+                  const Divider(
+                    height: 5,
+                    indent: 20,
+                    endIndent: 20,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                    child: Text("Please select gender"),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(50, 5, 50, 10),
+                    child: RadioListTile(
+                        title: Text("Male"),
+                        value: 'male',
+                        groupValue: _selectedGender,
+                        onChanged: (value) {
+                          setState(() {
+                            _selectedGender = "male";
+                          });
+                        }),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(50, 0, 50, 10),
+                    child: RadioListTile(
+                        title: Text("Female"),
+                        value: 'female',
+                        groupValue: _selectedGender,
+                        onChanged: (value) {
+                          setState(() {
+                            _selectedGender = "female";
+                          });
+                        }),
+                  ),
+                  const Divider(
+                    height: 5,
+                    indent: 20,
+                    endIndent: 20,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                    child: Text("Select level of activity"),
+                  ),
+                  DropdownButton(
+                    value: dropdownvalue,
+                    icon: Icon(Icons.keyboard_arrow_down),
+                    items: items.map((String items) {
+                      return DropdownMenuItem(value: items, child: Text(items));
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        dropdownvalue = "$value";
+                      });
+                    },
+                  ),
+                ],
               ),
             ),
           ),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 10),
-            child: Container(
-              height: 200,
-              color: Colors.grey[200],
-              child: Center(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(50),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Enter a search term',
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
+          ElevatedButton(onPressed: {
+            if (ageController >= 15) {_generateCalories} else {print("object")}
+          }, child: Text("Calculate"))
         ]),
       ),
     );
